d470da046c5e5068ec4da655bd93a85e4576203f
Lucas Macedo Rocha <cbp.lmd@gmail.com>
2024-03-18 21:54:32 -0300

    Commit

--- first_class.py
+++ first_class.py
@@ -1,4 +1,87 @@
-import pandas
 import pandas as pd
 
-df = pandas.read_csv()
+# Creating Data
+
+# df = pd.DataFrame({'Yes': [50, 21], 'No': [13, 1]})
+
+# df = pd.DataFrame({'Bob': ['I liked it', 'It was awful.'], 'Sue': ['Pretty good', 'Bland']})
+
+# df = pd.Series([1, 2, 3, 4, 5])
+
+# df = pd.Series([30, 45, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+
+# print(df)
+
+
+# Reading data files
+
+# data_review = pd.read_csv("wine_review.csv")
+
+# print(data_review.shape)  # The size of the file
+
+# print(data_review.head())   # First 5 rows
+
+# data_review = pd.read_csv("wine_review.csv", index_col=0)
+
+# print(data_review.head())
+
+
+# Native accessors
+
+data_review = pd.read_csv("wine_review.csv")
+
+# print(data_review.country)   Here we can specify the name of the column we
+#                             want to display
+
+# print(data_review['country'])     We can also do it like a dictionary
+# print(data_review['country'][0])    To get a single specific file we just need index it
+
+
+# Indexing in Pandas
+
+# print(data_review.iloc[0])     Here we are selecting the first row of data in DT
+
+# print(data_review.iloc[:, 0])   Here we are displaying the first column
+
+# print(data_review.iloc[:3, 0])    Here we are going from 0 to 3, but only going until 2
+
+# print(data_review.iloc[1:3, 0])     We can also do it like this
+
+# print(data_review.iloc[[0, 1, 2], 0])     We can also pass as a list
+
+# print(data_review.iloc[-5:])      Here we are getting the last 5
+
+
+# Label-based selection
+
+# print(data_review.loc[0, 'country'])
+
+# print(data_review.loc[:, ['taster_name', 'taster_twitter_handle', 'points']])
+
+
+# Manipulating the Index
+
+# print(data_review.set_index("title"))
+
+
+# Conditional Selection
+
+# print(data_review.country == 'Italy')
+
+# print(data_review.loc[data_review.country == 'Italy'])
+
+# print(data_review.loc[(data_review.country == 'Italy') & (data_review.points >= 90)])
+
+# print(data_review.loc[data_review.country.isin(['Italy', 'France'])])   # This allows you select data that
+#                                                                          is in a list of values
+
+# print(data_review.loc[data_review.price.notnull()])     # This allows us to get rid of the values that are null
+
+
+# Assigning data
+
+# data_review['critic'] = 'everyone'      # Assign a constant value
+# print(data_review['critic'])
+
+# data_review['index_backwards'] = range(len(data_review), 0, -1)   # Or with an iterable of values
+# print(data_review['index_backwards'])
